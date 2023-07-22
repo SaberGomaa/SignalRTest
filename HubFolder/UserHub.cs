7fb3be1ddc6e9f0c0bf20b926a2f06210743594c
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-22 22:16:27 +0300

    Change jQ Version And User Create RT

---
+++ HubFolder/UserHub.cs
@@ -0,0 +1,24 @@
+﻿//using Microsoft.AspNet.SignalR;
+//using Microsoft.AspNet.SignalR.Hubs;
+//using SignalRTest.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace SignalRTest.HubFolder
+//{
+//    [HubName("user")]
+//    public class UserHub : Hub
+//    {
+//        ChatDataBaseContext dbContext = new ChatDataBaseContext();
+
+//        public async void create(User user)
+//        {
+//            dbContext.Users.Add(user);
+//            dbContext.SaveChanges();
+
+//            Clients.All.newUser(user);
+//        }
+//    }
+//}
---
+++ UserHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using SignalRTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalRTest.HubFolder
+{
+    [HubName("user")]
+    public class UserHub : Hub
+    {
+        ChatDataBaseContext dbContext;
+
+        public UserHub()
+        {
+            dbContext = new ChatDataBaseContext();
+        }
+        public void create(User user)
+        {
+            dbContext.Users.Add(user);
+            dbContext.SaveChanges();
+
+            Clients.All.newUser(user);
+        }
+    }
+}
