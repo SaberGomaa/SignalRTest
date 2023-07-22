00f5ccc6f009e6beb8776667227df3caa0edd5ee
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-22 18:17:25 +0300

    Add StartUp And Map SignalR

--- Models/User.cs
+++ Models/User.cs
@@ -9,13 +9,9 @@ namespace SignalRTest.Models
     public partial class User
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public string Email { get; set; }
-
         public string Password { get; set; }
-
         public int Age { get; set; }
     }
 }
